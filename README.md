df670221cafe68f6c686b47346bd6a95bcf37b56
Mueez <mueez@askfortask.com>
2017-10-01 23:27:27 +0500

    adding readme.md and view changes

---
+++ README.md
@@ -0,0 +1,11 @@
+# tweet-reach
+A small application to find reach of a specific tweet
+
+## Steps to install
+* Run `composer install`
+* Add database credentials in `.env`
+* Add twitter app credentials in `.env`
+
+Run either `php artisan serve` or enable it using homestead
+
+Woah, enjoy calculating reach.
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -5,65 +5,11 @@
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>Twitter</title>
+        <title>Tweet Reach</title>
 
         <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta/css/bootstrap.min.css" integrity="sha384-/Y6pD6FV/Vv2HJnA6t+vslU6fwYXjCFtcEpHbNJ0lyAFsXTsjBbfaDjzALeQsN6M" crossorigin="anonymous">
-        <!-- Fonts -->
-        <link href="https://fonts.googleapis.com/css?family=Raleway:100,600" rel="stylesheet" type="text/css">
 
         <!-- Styles -->
-        {{--<style>
-            html, body {
-                background-color: #fff;
-                color: #636b6f;
-                font-family: 'Raleway', sans-serif;
-                font-weight: 100;
-                height: 100vh;
-                margin: 0;
-            }
-
-            .full-height {
-                height: 100vh;
-            }
-
-            .flex-center {
-                align-items: center;
-                display: flex;
-                justify-content: center;
-            }
-
-            .position-ref {
-                position: relative;
-            }
-
-            .top-right {
-                position: absolute;
-                right: 10px;
-                top: 18px;
-            }
-
-            .content {
-                text-align: center;
-            }
-
-            .title {
-                font-size: 84px;
-            }
-
-            .links > a {
-                color: #636b6f;
-                padding: 0 25px;
-                font-size: 12px;
-                font-weight: 600;
-                letter-spacing: .1rem;
-                text-decoration: none;
-                text-transform: uppercase;
-            }
-
-            .m-b-md {
-                margin-bottom: 30px;
-            }
-        </style>--}}
         <style>
             .full-height {
                 height: 100vh;
@@ -83,33 +29,36 @@
     <div class="container flex-center position-ref full-height">
         <div class="row">
             <div class="col-lg-12">
-                <div class="row"><div class="col-lg-12"> <div class="card">
-                    <div class="card-header"></div>
-                    <div class="card-body">
-                        @if ($errors->any())
-                            <div class="alert alert-danger">
-                                <ul>
-                                    @foreach ($errors->all() as $error)
-                                        <li>{{ $error }}</li>
-                                    @endforeach
-                                </ul>
+                <div class="row">
+                    <div class="col-lg-12">
+                        <div class="card">
+                            <div class="card-body">
+                                @if ($errors->any())
+                                    <div class="alert alert-danger">
+                                        <ul>
+                                            @foreach ($errors->all() as $error)
+                                                <li>{{ $error }}</li>
+                                            @endforeach
+                                        </ul>
+                                    </div>
+                                @endif
+                                <form action="{{route('calculate.reach')}}" method="post" class="form-inline">
+                                    {{csrf_field()}}
+                                    <div class="form-group">
+                                        <input type="text" class="form-control form-control-lg" placeholder="Enter Tweet url" name="tweetUrl" value="{{ old('tweetUrl') }}">
+                                    </div>
+                                    <button type="submit" class="btn-primary btn btn-lg">Submit</button>
+                                </form>
                             </div>
-                        @endif
-                        <form action="{{route('calculate.reach')}}" method="post" class="form-inline">
-                            {{csrf_field()}}
-                            <div class="form-group">
-                                <input type="text" class="form-control form-control-lg" placeholder="Enter Tweet url" name="tweetUrl" value="{{ old('tweetUrl') }}">
-                            </div>
-                            <button type="submit" class="btn-primary btn btn-lg">Submit</button>
-                        </form>
+                        </div>
                     </div>
-                </div></div> </div>
+                </div>
                 <div class="clearfix">&nbsp;</div>
                 <div class="row">
                     <div class="col-lg-12">
                         <div class="text-center">
                             @if (session('followersCount'))
-                                <h3>{{session('followersCount')}} People reach</h3>
+                                <h3>{{session('followersCount')}} people reach</h3>
                             @endif
                         </div>
                     </div>
